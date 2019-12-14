3849232eb7a8d11a93975501a17292404d8ad86b
= <=>
2019-12-14 10:11:07 +0800

    Start iPhiTech Workplace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Registration from './components/pages/Registration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+        <App />
+        
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/registration" component={Registration} />
+        </Switch>
+    </Router>, 
+document.getElementById('root'));
